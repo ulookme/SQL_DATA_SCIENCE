3e729acdf69ab94c95dae2399b847dda3110ffdf
charles <48330531+ulookme@users.noreply.github.com>
2020-07-07 10:46:51 +0200

    Add files via upload

---
+++ SQL/requeteB.sql
@@ -0,0 +1,32 @@
+SELECT rank, Major_code, Major, Major_category, total
+FROM recent_grads;
+
+SELECT Major, Employed
+FROM recent_grads
+WHERE Employed > 10000
+
+
+SELECT Major, Major_category, Employed, Unemployment_rate
+FROM recent_grads
+WHERE ( Major_category = 'Business' OR  Major_category = 'Arts'  OR  Major_category = ' Health ' )
+AND ( Employed > 20000   OR  Unemployment_rate < 0.051 );
+
+
+SELECT Major
+FROM recent_grads
+ORDER BY Major DESC
+LIMIT 10;
+
+SELECT College_jobs, Median, Unemployed
+FROM recent_grads
+LIMIT 20;
+
+SELECT Major, total, Median, Unemployment_rate, Major_category
+FROM recent_grads
+WHERE (Major_category != 'Engineering')  AND (Median <= 50000 OR Unemployed > 0.065) ;
+
+SELECT Major
+FROM recent_grads
+WHERE Major_category != 'Engineering'
+ORDER BY Major DESC
+LIMIT 20 ;
---
+++ SQL/statistiqueRequest/statistiqueRequest.sql
@@ -0,0 +1,18 @@
+SELECT min(population_growth)
+FROM facts;
+
+SELECT max(death_rate)
+FROM facts;
+
+SELECT sum(birth_rate)
+FROM facts;
+
+SELECT avg(birth_rate)
+FROM facts;
+
+SELECT sum(area_land)
+FROM facts;
+
+SELECT avg(area_water)
+FROM facts;
+
